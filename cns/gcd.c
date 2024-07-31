37445bf2d438216df5b0e81b51f214d9cfe72596
jenish barvaliya <jenishbarvaliya2012@gmail.com>
2024-07-31 12:16:56 +0530

    Update gcd.c

--- cns/gcd.c
+++ cns/gcd.c
@@ -4,12 +4,20 @@
 
 int main() {
     long long int num1,num2,a, b, temp,result;
-
+    
+    
     printf("Enter the first number: ");
     scanf("%lld", &num1);
     printf("Enter the second number: ");
     scanf("%lld", &num2);
-
+    
+    if (num2>num1){
+        temp=num1;
+        num1=num2;
+        num2=temp;
+        temp=0;
+    }
+    
     a = num1;
     b = num2;
 
